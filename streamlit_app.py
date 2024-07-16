8ab456a6625459b8c9e493b7ad3d6f80e58c679a
rafaelbabar <rafaelbabar@gmail.com>
2024-07-16 19:10:38 +0100

    login-app-files.py
    Checks username and password and if ,match found in csv file then the dashboard will allow the user to add another username and password to the csv file.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,44 @@
 import streamlit as st
+#D:\OneDrive\Desktop\Projects>streamlit run login-app-files.py
+def login():
+    st.title("Login")
+    enterusername=st.text_input("Please enter username to update system")
+    enterpassword=st.text_input("Please enter password",type="password")
+    if st.button("Check User"):
+        file=open("userlist.csv","r")
+        for line in file:
+            lines = line.split(",")
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+            username = lines[0]
+            password = lines[1]
+            if enterusername == username:
+                if enterpassword == password:
+                    st.session_state.logged_in=True
+                    st.success("Login Successful")              
+            else:
+                st.error("Invalid username or password")
+        file.close()
+def dashboard():      
+    st.write("Welcome Admin")
+    if st.button("Sign Out"):
+        st.session_state.logged_in=False
+    st.title("Dashboard")
+    username=st.text_input("Please username to add ")
+    password=st.text_input("Please enter password",type="password")
+    if st.button("Add User"):
+        file=open("userlist.csv","a")
+        file.write("\n")
+        file.write(username + "," + password)
+        file.close()
+        
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in=False
+
+if st.session_state.logged_in:
+    dashboard()
+else:
+    login()
+
+#Before coding - pip install streamlit
+#After coding - Run, cmd browse to file directory
+#Then streamlit run nameoffile.py
